b8e694934a134ea2cc1b60f3ad3f4c988b89ac14
ArturoMendozaLeeEBC <142127639+ArturoMendozaLeeEBC@users.noreply.github.com>
2023-09-09 18:19:47 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+
+function mostrarVentanaEmergente() {
+    var mensaje = "Bienvenido a Raíces Profundas, te invitamos a sembrar la semilla en tu vida. Gracias por visitar nuestra página web.";
+
+    alert(mensaje);
+}
+
+window.onload = mostrarVentanaEmergente;
